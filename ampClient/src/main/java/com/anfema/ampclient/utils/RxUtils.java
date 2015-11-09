7668691be684eb0316726d537e73d69576681a34
p-fischer <p.fischer@anfe.ma>
2015-11-09 13:22:42 +0100

    re #9 add convenience actions for on error and on complete

--- ampClient/src/main/java/com/anfema/ampclient/utils/RxUtils.java
+++ ampClient/src/main/java/com/anfema/ampclient/utils/RxUtils.java
@@ -3,6 +3,7 @@ package com.anfema.ampclient.utils;
 import retrofit.HttpException;
 import rx.Observable.Transformer;
 import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
 import rx.functions.Action1;
 import rx.schedulers.Schedulers;
 
@@ -20,6 +21,22 @@ public class RxUtils
 		Log.ex( exception );
 	};
 
+	/**
+	 * Convenience action for on error:
+	 * This is an action that receives Throwable but ignores exception. Use only if exception is handled at another spot.
+	 */
+	public static Action1<Throwable> IGNORE_ERROR = throwable -> {};
+
+	/**
+	 * Convenience action for on completed:
+	 * This is an action that receives no arguments and does nothing.
+	 */
+	public static Action0 NOTHING = () -> {};
+
+
+	/**
+	 * Use in compose operator to apply to every observable in the chain
+	 */
 	@SuppressWarnings("unchecked")
 	public static <T> Transformer<T, T> applySchedulers()
 	{
