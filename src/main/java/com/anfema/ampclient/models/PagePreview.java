2d4a04da3f4bd0504a7d6a63cdf51d43fce1d107
p-fischer <p.fischer@anfe.ma>
2015-12-07 16:18:33 +0100

    re #58 parse meta information in page previes

--- src/main/java/com/anfema/ampclient/models/PagePreview.java
+++ src/main/java/com/anfema/ampclient/models/PagePreview.java
@@ -2,6 +2,8 @@ package com.anfema.ampclient.models;
 
 import org.joda.time.DateTime;
 
+import java.util.Map;
+
 /**
  * Meta/preview information for pages. It does not hold the content for a page.
  */
@@ -23,14 +25,23 @@ public class PagePreview
 	public DateTime last_changed;
 
 	/**
-	 * page's title can be displayed in a preview
+	 * Meta contains additional, flexible data.
+	 * Since it is very application-specific the provided data is stored as key-value-pairs, simply consisting of strings.
 	 */
-	public String title;
+	public Map<String, String> meta;
+
 
 	/**
-	 * image url for thumbnail to display in a preview
+	 * Convenience method to compare values from meta with other ones.
+	 *
+	 * @param metaKey      The key of meta value (e.g. "title" or "thumbnail")
+	 * @param compareValue A value to compare the concerning meta value with.
+	 * @return true, if meta value exists and equals {@param compareValue}
 	 */
-	public String thumbnail;
+	public boolean metaEquals( String metaKey, String compareValue )
+	{
+		return meta != null && meta.containsKey( metaKey ) && meta.get( metaKey ).equals( compareValue );
+	}
 
 	/**
 	 * the page's layout
@@ -40,6 +51,6 @@ public class PagePreview
 	@Override
 	public String toString()
 	{
-		return "PagePreview [identifier = " + identifier + ", parent = " + parent + ", last_changed = " + last_changed + ", title = " + title + ", thumbnail = " + thumbnail + ", layout = " + layout + "]";
+		return "PagePreview [identifier = " + identifier + ", parent = " + parent + ", last_changed = " + last_changed + ", layout = " + layout + "]";
 	}
 }
