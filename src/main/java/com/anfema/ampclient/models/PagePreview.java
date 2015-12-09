0acff824f2146ceb1fb345bc1ea8d6b0bbd37744
p-fischer <p.fischer@anfe.ma>
2015-12-09 15:06:35 +0100

    re #58 topics can come as comma-separated string or as array, handle by lazy parsing meta values on request

--- src/main/java/com/anfema/ampclient/models/PagePreview.java
+++ src/main/java/com/anfema/ampclient/models/PagePreview.java
@@ -1,7 +1,14 @@
 package com.anfema.ampclient.models;
 
+import com.anfema.ampclient.serialization.GsonHolder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
+import com.google.gson.reflect.TypeToken;
+
 import org.joda.time.DateTime;
 
+import java.lang.reflect.Type;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -28,8 +35,20 @@ public class PagePreview
 	 * Meta contains additional, flexible data.
 	 * Since it is very application-specific the provided data is stored as key-value-pairs, simply consisting of strings.
 	 */
-	public Map<String, String> meta;
+	public Map<String, JsonElement> meta;
 
+	public String getMetaString( String metaKey ) throws JsonSyntaxException
+	{
+		return GsonHolder.getInstance().fromJson( meta.get( metaKey ), String.class );
+	}
+
+	public List<String> getMetaList( String metaKey ) throws JsonSyntaxException
+	{
+		Type listType = new TypeToken<List<String>>()
+		{
+		}.getType();
+		return GsonHolder.getInstance().fromJson( meta.get( metaKey ), listType );
+	}
 
 	/**
 	 * Convenience method to compare values from meta with other ones.
@@ -40,7 +59,19 @@ public class PagePreview
 	 */
 	public boolean metaEquals( String metaKey, String compareValue )
 	{
-		return meta != null && meta.containsKey( metaKey ) && meta.get( metaKey ).equals( compareValue );
+		if ( meta == null || !meta.containsKey( metaKey ) )
+		{
+			return false;
+		}
+
+		try
+		{
+			return getMetaString( metaKey ).equals( compareValue );
+		}
+		catch ( JsonSyntaxException e )
+		{
+			throw new JsonSyntaxException( "You tried to compare " + compareValue + " with a meta entry " + meta.get( metaKey ) + " which could not be parsed to a String.", e );
+		}
 	}
 
 	/**
