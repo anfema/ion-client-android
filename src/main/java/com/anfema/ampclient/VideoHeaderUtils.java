e0cb89717f33fabcb187c7f178628a98ab2d41e8
p-fischer <p.fischer@anfe.ma>
2016-01-15 16:36:40 +0100

    re #57 - extended documentation, because it is critical when to call this method

--- src/main/java/com/anfema/ampclient/VideoHeaderUtils.java
+++ src/main/java/com/anfema/ampclient/VideoHeaderUtils.java
@@ -9,11 +9,16 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This utility methods are only relevant if minimum supported API level is below 21. Otherwise, headers can be set with {@link VideoView#setVideoURI(Uri, Map)}.
+ */
 public class VideoHeaderUtils
 {
 	/**
 	 * Setting headers for video URI in {@link VideoView} is possible with method {@link VideoView#setVideoURI(Uri, Map)} from API level 21.
 	 * This method is a bit hacky. It enables setting headers by use reflection to access a private field.
+	 * <p/>
+	 * Call this method AFTER {@link VideoView#setVideoURI(Uri)} because otherwise "mHeaders" field is set to null again!
 	 */
 	public static void setHeaders( VideoView videoView, Map<String, String> headersMap )
 	{
@@ -31,6 +36,8 @@ public class VideoHeaderUtils
 
 	/**
 	 * Set authorization header for video URI in {@link VideoView}.
+	 * <p/>
+	 * Call this method AFTER {@link VideoView#setVideoURI(Uri)} because otherwise "mHeaders" field is set to null again!
 	 *
 	 * @see #setHeaders(VideoView, Map)
 	 */
