f7586c4db657fdf9001c10a4755766c9a2926dd9
p-fischer <p.fischer@anfe.ma>
2016-01-21 18:48:05 +0100

    re #69 fix PageUrls: append locale query (otherwise caching would not work anymore)

--- src/main/java/com/anfema/ampclient/pages/PagesUrls.java
+++ src/main/java/com/anfema/ampclient/pages/PagesUrls.java
@@ -7,11 +7,16 @@ public class PagesUrls
 {
 	public static String getCollectionUrl( AmpConfig config )
 	{
-		return config.baseUrl + AmpCallType.COLLECTIONS.toString() + FileUtils.SLASH + config.collectionIdentifier;
+		return config.baseUrl + AmpCallType.COLLECTIONS.toString() + FileUtils.SLASH + config.collectionIdentifier + getLocaleQuery( config );
 	}
 
 	public static String getPageUrl( AmpConfig config, String pageId )
 	{
-		return config.baseUrl + AmpCallType.PAGES.toString() + FileUtils.SLASH + config.collectionIdentifier + FileUtils.SLASH + pageId;
+		return config.baseUrl + AmpCallType.PAGES.toString() + FileUtils.SLASH + config.collectionIdentifier + FileUtils.SLASH + pageId + getLocaleQuery( config );
+	}
+
+	private static String getLocaleQuery( AmpConfig config )
+	{
+		return "?locale=" + config.locale;
 	}
 }
