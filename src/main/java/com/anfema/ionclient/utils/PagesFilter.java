5ab07222809ea7078cfe95be18bf4fadd9ff2d61
p-fischer <p.fischer@anfe.ma>
2016-03-09 15:22:07 +0100

    re #36 - write a comment in PagesFIlter

--- src/main/java/com/anfema/ionclient/utils/PagesFilter.java
+++ src/main/java/com/anfema/ionclient/utils/PagesFilter.java
@@ -6,9 +6,19 @@ import java.util.List;
 
 import rx.functions.Func1;
 
+/**
+ * This utility filters are supposed to be used with
+ * {@link com.anfema.ionclient.pages.IonPages#getPagePreviews(Func1)},
+ * {@link com.anfema.ionclient.pages.IonPages#getPagePreviewsSorted(Func1)},
+ * {@link com.anfema.ionclient.pages.IonPages#getPages(Func1)}, and
+ * {@link com.anfema.ionclient.pages.IonPages#getPagesSorted(Func1)}
+ */
 public class PagesFilter
 {
-
+	/**
+	 * Before using this filter method, consider using
+	 * {@link com.anfema.ionclient.pages.IonPages#getPage(String)} or {@link com.anfema.ionclient.pages.IonPages#getPagePreview(String)} instead.
+	 */
 	public static Func1<PagePreview, Boolean> identifierEquals( String pageIdentifier )
 	{
 		return pagePreview -> pagePreview.identifier != null && pagePreview.identifier.equals( pageIdentifier );
@@ -17,6 +27,11 @@ public class PagesFilter
 	public static Func1<PagePreview, Boolean> identifierIn( List<String> pageIdentifiers )
 	{
 		return pagePreview -> {
+			if ( pageIdentifiers == null )
+			{
+				return false;
+			}
+
 			for ( String pageIdentifier : pageIdentifiers )
 			{
 				if ( pageIdentifier != null && pageIdentifier.equals( pagePreview.identifier ) )
