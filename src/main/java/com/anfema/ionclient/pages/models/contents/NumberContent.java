b700d6112e1438397ea0d2b752be5546bc76855b
p-fischer <p.fischer@anfe.ma>
2017-02-13 13:38:38 +0100

    NumberContent: there was a bug fix in ION. Number content now sends decimal_places which is an exponent of 10. Thus, the actual value is calculated from value and decimal_places

--- src/main/java/com/anfema/ionclient/pages/models/contents/NumberContent.java
+++ src/main/java/com/anfema/ionclient/pages/models/contents/NumberContent.java
@@ -2,5 +2,11 @@ package com.anfema.ionclient.pages.models.contents;
 
 public class NumberContent extends Content
 {
-	public float value;
+	private float value;
+	private int   decimal_places;
+
+	public float getValue()
+	{
+		return ( float ) ( value / Math.pow( 10, decimal_places ) );
+	}
 }
