2c62d15014c0c8eaa09ee12c6e29ecb4fcb547de
Peter Fischer <p.fischer@anfe.ma>
2021-09-21 16:23:33 +0200

    Refactor: Convert ContainerContent to data class and use Parcelize annotation
    generating the parcelable implementation

--- src/main/java/com/anfema/ionclient/pages/models/contents/ContainerContent.kt
+++ src/main/java/com/anfema/ionclient/pages/models/contents/ContainerContent.kt
@@ -1,31 +1,8 @@
 package com.anfema.ionclient.pages.models.contents
 
-import android.os.Parcel
-import android.os.Parcelable
-import java.util.ArrayList
+import kotlinx.parcelize.Parcelize
 
-open class ContainerContent : Content {
-
-    @JvmField
-    var children: ArrayList<Content>? = null
-
-    constructor()
-
-    protected constructor(`in`: Parcel?) : super(`in`) {
-        // TODO write logic to read children. (recover type + element)
-    }
-
-    override fun writeToParcel(dest: Parcel, flags: Int) {
-        super.writeToParcel(dest, flags)
-        // TODO write logic to parcel children. (Save element + type)
-    }
-
-    override fun describeContents() = 0
-
-    companion object CREATOR : Parcelable.Creator<ContainerContent?> {
-
-        override fun createFromParcel(source: Parcel) = ContainerContent(source)
-        override fun newArray(size: Int): Array<ContainerContent?> = arrayOfNulls(size)
-    }
-}
-}
+@Parcelize
+data class ContainerContent(
+    val children: List<Content>,
+) : Content()
