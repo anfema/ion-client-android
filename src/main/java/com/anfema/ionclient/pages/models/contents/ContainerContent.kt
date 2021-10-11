a806cdb4d7a497bb842f410763ba91085b325dad
Peter Fischer <p.fischer@anfe.ma>
2021-10-11 17:47:45 +0200

    Revert "Enhancement: Add subtype to ContainerContent"
    This reverts commit d201512f1b6c07ee9cdaef0735cf473e466f4ace.

--- src/main/java/com/anfema/ionclient/pages/models/contents/ContainerContent.kt
+++ src/main/java/com/anfema/ionclient/pages/models/contents/ContainerContent.kt
@@ -1,24 +1,8 @@
 package com.anfema.ionclient.pages.models.contents
 
-import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
-
-enum class Subtype {
-    @SerializedName("generic")
-    Generic,
-
-    @SerializedName("list")
-    List,
-
-    @SerializedName("structblock")
-    StructBlock,
-
-    @SerializedName("streamblock")
-    StreamBlock,
-}
 
 @Parcelize
 data class ContainerContent(
     val children: List<Content>,
-    val subtype: Subtype,
 ) : Content()
