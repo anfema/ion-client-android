827515551a8ca36a137f4498190092c4658e8e33
Peter Fischer <p.fischer@anfe.ma>
2022-03-04 15:46:07 +0100

    Refactor: Convert PageCacheIndex to Kotlin

---
+++ src/main/java/com/anfema/ionclient/caching/index/PageCacheIndex.kt
@@ -0,0 +1,39 @@
+package com.anfema.ionclient.caching.index
+
+import android.content.Context
+import com.anfema.ionclient.IonConfig
+import com.anfema.ionclient.caching.index.PageCacheIndex
+import com.anfema.ionclient.pages.IonPageUrls
+import com.anfema.ionclient.pages.models.Page
+import org.joda.time.DateTime
+
+class PageCacheIndex(
+    /**
+     * Last changed info received from server in the page response
+     */
+    private val lastChanged: DateTime,
+) : CacheIndex() {
+
+    fun isOutdated(serverDate: DateTime?): Boolean =
+        lastChanged.isBefore(serverDate)
+
+    companion object {
+
+        @JvmStatic
+        fun retrieve(requestUrl: String?, config: IonConfig?, context: Context?): PageCacheIndex {
+            return CacheIndexStore.retrieve(requestUrl, PageCacheIndex::class.java, config, context)
+        }
+
+        @JvmStatic
+        fun save(page: Page, config: IonConfig?, context: Context?) {
+            save(page.identifier, page.last_changed, config, context)
+        }
+
+        @JvmStatic
+        fun save(pageIdentifier: String?, pageLastChanged: DateTime, config: IonConfig?, context: Context?) {
+            val url = IonPageUrls.getPageUrl(config, pageIdentifier)
+            val cacheIndex = PageCacheIndex(pageLastChanged)
+            CacheIndexStore.save(url, cacheIndex, config, context)
+        }
+    }
+}
