2540431bb15f4797d4a8e160675f23b0078b0002
Peter Fischer <p.fischer@anfe.ma>
2022-04-03 00:42:31 +0200

    Refactor: Replace custom Basic Auth implementation with the one built-into OkHttp
    Add deprecation message

--- src/main/java/com/anfema/ionclient/authentication/BasicAuth.kt
+++ src/main/java/com/anfema/ionclient/authentication/BasicAuth.kt
@@ -1,8 +1,6 @@
 package com.anfema.ionclient.authentication
 
-import android.util.Base64
-import com.anfema.ionclient.utils.IonLog
-import java.io.UnsupportedEncodingException
+import okhttp3.Credentials
 
 /**
  * Helper class for basic access authentication.
@@ -12,15 +10,8 @@ object BasicAuth {
      * Generates the value for the "Authorization" header.
      */
     @JvmStatic
-    fun getAuthHeaderValue(username: String, password: String): String {
-        val usernamePassword = "$username:$password"
-        var data: ByteArray? = null
-        try {
-            data = usernamePassword.toByteArray(charset("UTF-8"))
-        } catch (e: UnsupportedEncodingException) {
-            IonLog.ex(Exception("Could not get bytes of $usernamePassword", e))
-        }
-        val base64 = Base64.encodeToString(data, Base64.NO_WRAP)
-        return "Basic $base64"
-    }
+    @Deprecated("Use implementation from OkHttp",
+        ReplaceWith("Credentials.basic(username, password)", "okhttp3.Credentials"))
+    fun getAuthHeaderValue(username: String, password: String): String =
+        Credentials.basic(username, password)
 }
