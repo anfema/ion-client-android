a1d4cd2b96cbd085da634a3f770c795fef00f6b7
Peter Fischer <p.fischer@anfe.ma>
2022-04-06 16:08:35 +0200

    Chore: Remove check for old Android SDK version

--- src/main/java/com/anfema/ionclient/utils/MemoryUtils.java
+++ src/main/java/com/anfema/ionclient/utils/MemoryUtils.java
@@ -5,8 +5,6 @@ import android.content.Context;
 
 import static android.content.Context.ACTIVITY_SERVICE;
 import static android.content.pm.ApplicationInfo.FLAG_LARGE_HEAP;
-import static android.os.Build.VERSION.SDK_INT;
-import static android.os.Build.VERSION_CODES.HONEYCOMB;
 
 public class MemoryUtils
 {
@@ -17,7 +15,7 @@ public class MemoryUtils
 	{
 		ActivityManager am = ( ActivityManager ) context.getSystemService( ACTIVITY_SERVICE );
 		boolean largeHeap = ( context.getApplicationInfo().flags & FLAG_LARGE_HEAP ) != 0;
-		int memoryClass = largeHeap && SDK_INT >= HONEYCOMB ? am.getLargeMemoryClass() : am.getMemoryClass();
+		int memoryClass = largeHeap ? am.getLargeMemoryClass() : am.getMemoryClass();
 		IonLog.i( "Memory Cache", "available Cache: " + memoryClass + " MB" );
 		return 1024 * 1024 * memoryClass;
 	}
