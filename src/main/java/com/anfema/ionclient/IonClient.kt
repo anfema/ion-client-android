bad4e65f36dca6f1544a0d812aad5212d18807d5
Peter Fischer <p.fischer@anfe.ma>
2022-04-11 09:09:57 +0200

    Refactor: Extract CachingStrategy to own file

--- src/main/java/com/anfema/ionclient/IonClient.kt
+++ src/main/java/com/anfema/ionclient/IonClient.kt
@@ -19,27 +19,6 @@ import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 
 /**
- * Defines strategies, when to fetch data from cache and when to download it from internet.
- */
-enum class CachingStrategy {
-    /**
-     * strategy:
-     * 1. fetch current version from cache
-     * 2. download current version (if connected to internet)
-     * 3. fetch possibly outdated version from cache (if it exists)
-     * 4. error (because no version in cache exists and no internet connection)
-     */
-    NORMAL,
-
-    /**
-     * strategy:
-     * 1. fetch (possibly outdated) version from cache (if it exists)
-     * 2. error (because no version in cache exists and downloading is prohibited with this mode)
-     */
-    STRICT_OFFLINE
-}
-
-/**
  * Main entry point for ION functionality.
  * Serving as entry point IonClient holds interfaces providing the actual implementation of its functionality.
  *
